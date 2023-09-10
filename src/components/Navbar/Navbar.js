8d8a753eee1d84e60868729d7b0c1ed82a54c599
jimmyatv <rasic.alexandar@gmail.com>
2023-09-11 00:26:53 +0200

    Header 100%

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,68 +1,87 @@
 import React from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
-import {ImLocation2} from 'react-icons/im';
-import {BiPhone} from 'react-icons/bi';
-import {BsEnvelope, BsEye} from 'react-icons/bs';
+import header from '../../assets/header.png'
+import { FaLocationDot, FaPhone, FaRegEnvelope, FaRegEye } from "react-icons/fa6";
 
 const Navbar = () => {
 
 
 
-  return (
-    <div className='navBarMain container'>
-        {/* TOP MENU */}
-        <div className='topMenu row'>
-            <div className='col-md-3'>
-                <img className='logoImg' src={logo} alt=''  />
-            </div>
-            <div className='col-md-3'>
-                <ImLocation2 style={{color:'var(--primary)', fontSize:'22px', marginRight:'5px'}} />
-                <span>7041 Grand national drive, suite 230, Orlando, FL</span>
-            </div>
-            <div className='col-md-3'>
-                <BiPhone style={{color:'var(--primary)', fontSize:'22px', marginRight:'5px'}} />
-                <span>7041 Grand national drive, suite 230, Orlando, FL</span>
-            </div>
-            <div className='col-md-3'>
-                <BsEnvelope style={{color:'var(--primary)', fontSize:'22px', marginRight:'5px'}} />
-                <span>7041 Grand national drive, suite 230, Orlando, FL</span>
-            </div>
-        </div>
-        {/* NAV BAR */}
-        <div className='navBar'>
-            <img src='logo' alt='' />
-            <ul className='navBarMenu'>
-                <li>
-                    <a href='#'>Home</a>
-                </li>
-                <li>
-                    <a href='#'>Covid-19</a>
-                </li>
-                <li>
-                    <a href='#'>Services</a>
-                </li>
-                <li>
-                    <a href='#'>News</a>
-                </li>
-
-                <li>
-                    <a href='#'>About</a>
-                </li>
-                <li>
-                    <a href='#'>Properties</a>
-                </li>
-                <li>
-                    <a href='#'>Owners</a>
-                </li>
-                <li>
-                    <a href='#'><BsEye style={{color:'var(--primary)', fontSize:'22px'}} /></a>
-                </li>
-            </ul>
+    return (
+        <div className='navBarMain'>
             
+            <div className='container'>
+                {/* TOP MENU */}
+                <div className='topMenu row'>
+                    <div className='col-md-3'>
+                        <img className='logoImg' src={logo} alt='' />
+                    </div>
+                    <div className='col-md-3'>
+                        <FaLocationDot style={{ color: 'var(--primary)', fontSize: '22px', marginRight: '5px' }} />
+                        <span>7041 Grand national drive, suite 230, Orlando, FL</span>
+                    </div>
+                    <div className='col-md-3'>
+                        <FaPhone style={{ color: 'var(--primary)', fontSize: '22px', marginRight: '5px' }} />
+                        <span>7041 Grand national drive, suite 230, Orlando, FL</span>
+                    </div>
+                    <div className='col-md-3'>
+                        <FaRegEnvelope style={{ color: 'var(--primary)', fontSize: '22px', marginRight: '5px' }} />
+                        <span>7041 Grand national drive, suite 230, Orlando, FL</span>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <div className='container'>
+                {/* NAV BAR */}
+                <div className='navBar'>
+                    <img src='logo' alt='' />
+                    <ul className='navBarMenu'>
+                        <li>
+                            <a href='#'>Home</a>
+                        </li>
+                        <li>
+                            <a href='#'>Covid-19</a>
+                        </li>
+                        <li>
+                            <a href='#'>Services</a>
+                        </li>
+                        <li>
+                            <a href='#'>News</a>
+                        </li>
+
+                        <li>
+                            <a href='#'>About</a>
+                        </li>
+                        <li>
+                            <a href='#'>Properties</a>
+                        </li>
+                        <li>
+                            <a href='#'>Owners</a>
+                        </li>
+                        <li>
+                            <a href='#'><FaRegEye style={{ color: 'var(--primary)', fontSize: '22px' }} /></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            {/* HEADER PHOTO */}
+            <div className='relative'>
+                <img src={header} alt='' />
+                <div className='header-photo text-center'>
+                    <h1>Reimagining Resorts A Fresh Perspective</h1>
+                    <button className='my-btn'>Learn More</button>
+                </div>
+            </div>
+            {/* PARTNERS */}
+            <div className='partners'>
+                <div className='text-center'>
+                    <h4>“We partner with owners and boards to provide a truly customized approach.”</h4>
+                    <p className='text-uppercase'>- Alexander Krakovsky, ceo</p>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 };
 
 export default Navbar;
